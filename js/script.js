a8535ffe28cecfa304023d6951fc8b243e2edfbf
Alexksxx <bestuzhev1997@mail.ru>
2022-11-21 15:11:49 +0300

    update: add variables

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ document.getElementById('out').innerHTML = '<b>Fat content</b>'; // позвол
 
 document.querySelector('.class-one').innerHTML = 'Change by class'; // работает только на одном, первом попавшемся по ходу чтения кода
 document.querySelector('#one').innerHTML = 'Change by id'; // так как функция более гибкая, нужно обозначение селектора: id - #, класс - . и тд
+
+let a;
+a = document.querySelector('.paragraph');
+a.innerHTML = 2110983;
+
+let b = document.getElementById('two');
+b.innerHTML = 'lorem';
