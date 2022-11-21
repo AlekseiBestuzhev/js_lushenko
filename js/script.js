54a6bf394686cd03bd8e0d1f443756ce93fc5ca5
Alexksxx <bestuzhev1997@mail.ru>
2022-11-21 14:49:30 +0300

    update: add function querryselection

--- js/script.js
+++ js/script.js
@@ -6,3 +6,6 @@ console.log('Hello' + ' ' + 'world');
 // alert('Hello'); 
 document.getElementById('out').innerHTML = 'Second content'; // позволяет менять контент по id. getElementById - нашли элемент, innerHTML - заменили содержимое
 document.getElementById('out').innerHTML = '<b>Fat content</b>'; // позволяет добавлять все, даже теги
+
+document.querySelector('.class-one').innerHTML = 'Change by class'; // работает только на одном, первом попавшемся по ходу чтения кода
+document.querySelector('#one').innerHTML = 'Change by id'; // так как функция более гибкая, нужно обозначение селектора: id - #, класс - . и тд
