2a2f970bb92e15630513f8cbbe0b1ff3a861b01d
Alexksxx <bestuzhev1997@mail.ru>
2022-11-21 14:13:08 +0300

    update: change html content from js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ console.log('300');
 console.log(300); // в консоле показывается синим цветом
 console.info("Hello");
 console.log('Hello' + ' ' + 'world');
-alert('Hello');
+// alert('Hello'); 
+document.getElementById('out').innerHTML = 'Second content'; // позволяет менять контент по id 
