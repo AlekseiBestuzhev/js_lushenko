3c4749798c2fad46f36182e7106207b1931af12e
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-02 22:11:59 +0300

    update: unit_07 complete

--- unit_07/unit_07.js
+++ unit_07/unit_07.js
@@ -122,9 +122,17 @@ document.querySelector('.b-9').onclick = function () {
 // Создайте функцию t10, которая принимает 2 числа и возвращает большее из них.В случае равенства - любое из чисел.
 
 function t10(a, b) {
-
+	if (a > b) {
+		return a;
+	}
+	else if (a < b) {
+		return b;
+	}
+	else {
+		return a;
+	}
 }
 
 document.querySelector('.b-10').onclick = function () {
-	document.querySelector('.out-10').textContent = t10(3, 9);
+	document.querySelector('.out-10').textContent = t10(3, 7);
 }
