a757e388984ea4fb4348cb4d1061c0881c2f4835
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-21 23:07:33 +0400

    update: unit_12 task_11 complete

--- unit_12/unit_12.js
+++ unit_12/unit_12.js
@@ -186,6 +186,12 @@ let a11 = [
 
 function f11() {
 	let out = '';
+	for (let i = 0; i < a11.length; i++) {
+		for (let k = a11[i].length - 1; k >= 0; k--) {
+			out += a11[i][k] + ' ';
+		}
+	}
+	document.querySelector('.out-11').textContent = out;
 }
 
 document.querySelector('.b-11').onclick = f11;
