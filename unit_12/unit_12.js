490dd3a155f80d8f531e9a27678e5e1e4c7ea4e3
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-22 21:27:50 +0400

    update: unit_12 complete

--- unit_12/unit_12.js
+++ unit_12/unit_12.js
@@ -384,12 +384,15 @@ console.groupEnd();
 //  Т.е вы руками просто пишите массив такой, чтобы условия выполнялись.
 
 let a20 = [
-
+	0,
+	[[0, 1, 3], [12, 9, 7]],
+	[[0, 1, 3], 1, 18],
+	12
 ];
 
 console.group('Task 20 ================');
-// console.log(a20[1][1][1] == 9);
-// console.log(a20[2][2] == 18);
-// console.log(a20[3]== 12);
+console.log(a20[1][1][1] == 9);
+console.log(a20[2][2] == 18);
+console.log(a20[3] == 12);
 
 console.groupEnd();
